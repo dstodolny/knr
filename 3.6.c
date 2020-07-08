fc22ab48b1b4a2c64f2843cb5bca1cbe56d3697d
Dominik Stodolny <dominik@stodolny.org>
2020-07-08 19:58:16 +0100

    solve 3.6

---
+++ 3.6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+int abs(int x)
+{
+    if (x < 0)
+        return -x;
+    else
+        return x;
+}
+
+
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/*
+  The original version of itoa doesn't work for INT_MIN because negating
+  INT_MIN results in INT_MIN.
+*/
+void itoa(int n, char s[], int w)
+{
+    int i, sign;
+
+    if ((sign = n) < 0)
+        n = -n;
+    i = 0;
+    do {
+        s[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+    if (sign < 0)
+        s[i++] = '-';
+    while (i<w)
+        s[i++] = ' ';
+    s[i] = '\0';
+    reverse(s);
+}
+
+int main()
+{
+    char buf[36];
+    itoa(123, buf, 6);
+    printf("%s\n", buf);
+    return 0;
+}
