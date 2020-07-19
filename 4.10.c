9d89f0300b7c7d9ffc97fb05084cf8510aea79d8
Dominik Stodolny <dominik@stodolny.org>
2020-07-19 11:46:44 +0100

    solve 4.10

---
+++ 4.10.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include "lib/knr.h"
+
+#define MAXOP   100
+#define MAXVAL  100
+#define NUMBER  '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+int sp = 0;
+double val[MAXVAL];
+int li = 0;
+char line[MAXLINE];
+
+void push(double f)
+{
+    if(sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+int getop(char s[])
+{
+    int c, i;
+
+    if (line[li] == '\0')
+        if (_getline(line, MAXLINE) == 0)
+            return EOF;
+        else
+            li = 0;
+    while ((s[0] = c = line[li++]) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = line[li++]))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = line[li++]))
+            ;
+    s[i] = '\0';
+    li--;
+    return NUMBER;
+}
+
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            if (op2 != 0)
+                push(fmod(pop(), op2));
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
